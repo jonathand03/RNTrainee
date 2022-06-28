e4b7eaed3482aee653a7d5f5ef8a114c6f1602fc
Jonathan Drumond <jonathandrumond03@gmail.com>
2022-06-27 22:11:50 -0300

    Aprendizado de useState concluido

--- Vacinadx/src/App.tsx
+++ Vacinadx/src/App.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import React,{useState} from 'react';
+import { Button, SafeAreaView, StyleSheet, Text } from 'react-native';
+
 import  Header  from '../src/components/Header/index';
 
+
+
 const App : React.FC = () =>{
+  const [name,setName] = useState<string>('Jonathan');
+  const [title,setTitle] = useState<string>('Olá ');
+  const [count, setCount] = useState<number>(0);
+
+  const handlePressButton = () =>{
+    name == 'Jonathan' ? setName('Paulo') : setName('Jonathan');
+  }
+
+  const handlePressCountButton = () =>{
+    count == 30 ? setCount(0) : setCount(count+1);
+  }
+
   return(
     <SafeAreaView style={style.App}>
-      <Header welcomeMessage='Bem-vindo!' name='Jonatha Drumond'></Header>
+      <Header welcomeMessage='Welcome, ' name={name}></Header>
+      <Text style={style.Textcolor}>{count}</Text>
+      <Button title='Change name' onPress={handlePressCountButton}></Button>
     </SafeAreaView>
   );
 };
@@ -20,4 +37,8 @@ const style = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center'
   },
+  Textcolor:{
+    color: 'white',
+    fontSize: 20
+  }
 });
